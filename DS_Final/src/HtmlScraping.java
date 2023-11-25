5e279466803585e57cc73b699bdd3a48ea98b6b9
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-11-25 13:28:39 +0800

    update HtmlScraping class- HW5

--- DS_Final/src/HtmlScraping.java
+++ DS_Final/src/HtmlScraping.java
@@ -1,4 +1,33 @@
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Stack;
 
-public class HtmlScraping {
+public class HtmlScraping
+{
+	private String urlStr;
+	private String content;
 
+	public HtmlScraping(String urlStr)
+	{
+		this.urlStr = urlStr;
+	}
+
+	private String fetchContent() throws IOException
+	{
+		URL url = new URL(this.urlStr);
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		String retVal = "";
+
+		String line = null;
+		while ((line = br.readLine()) != null)
+		{
+			retVal = retVal + line + "\n";
+		}
+
+		return retVal;
+	}
 }
