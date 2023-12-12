d70b19eaf78b1195f7fdc97b59dea75ab597b511
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-12-12 14:02:56 +0800

    update Main class

--- DS_Final/src/Main.java
+++ DS_Final/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,19 +15,19 @@ public class Main
 		{
 			File file = new File("input.txt");
 			Scanner fileSc = new Scanner(file);
-			KeywordList kLst = new KeywordList();
+			ArrayList keywords = new ArrayList<Keyword>();
 			
 			if(fileSc.hasNext()) {
 				String name = fileSc.next();
         		int weight = fileSc.nextInt();
         		Keyword keyword = new Keyword(name, 0, weight);
-        		kLst.add(keyword);
+        		keywords.add(keyword);
 			}
 			
 			System.out.println("Enter Keyword: ");
 			Scanner sc = new Scanner(System.in);
 			String keyword = sc.next();
-			System.out.println(new GoogleQuery(keyword).query());
+			System.out.println(new GoogleQuery(keyword, keywords).query());
 		} 
 		catch (IOException e) 
 		{
