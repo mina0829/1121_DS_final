9a668e04db75c3e2bd821a1f6977867be66b9b4f
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-12-18 17:35:36 +0800

    update

--- DS_Final/src/Main.java
+++ DS_Final/src/Main.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/TestProject")
+@WebServlet("/Main")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Keyword> keywords;
