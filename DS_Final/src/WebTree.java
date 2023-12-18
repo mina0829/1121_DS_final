aa4ed356cd50e2e5f4570fe34337988afde8c8f6
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-12-18 18:55:06 +0800

    新增註解

--- DS_Final/src/WebTree.java
+++ DS_Final/src/WebTree.java
@@ -9,7 +9,8 @@ public class WebTree
 	{
 		this.root = new WebNode(rootPage);
 	}
-
+	
+	//從root開始計算分數
 	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException
 	{
 		setPostOrderScore(root, keywords);
@@ -30,6 +31,7 @@ public class WebTree
 		return retVal;
 	}
 	
+	//比較分數大小，排續有錯可調整這
 	public int compareTo(WebTree tree) {
 		return Double.compare(this.root.nodeScore, tree.root.nodeScore);
     }
