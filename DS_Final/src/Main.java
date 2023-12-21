255a161f9f25904e6ee6fa77a3f61a6ba8bad9b0
Linyi029 <118969781+Linyi029@users.noreply.github.com>
2023-12-21 17:00:13 +0800

    Update Main.java
    新增可能的做法
    1. query改為ArrayList型態（hashmap無序），有試過但可能是googlequery那邊沒改好，現在只有一筆會加到webTreeList（已排序過的ArrayList<WebTree>）
    2. 使用Treemap，有試過且成功，但感覺有點不好（他會自己排序，不知道允不允許使用）

--- DS_Final/src/Main.java
+++ DS_Final/src/Main.java
@@ -27,7 +27,7 @@ public class Main extends HttpServlet {
     public Main() {
         super();
         // TODO Auto-generated constructor stub
-        try 
+        /*try 
 		{
 			File file = new File("input.txt");
 			Scanner fileSc = new Scanner(file);
@@ -42,7 +42,7 @@ public class Main extends HttpServlet {
 		}catch(Exception e) {
 			
 		}
-    }
+    }*/
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -58,10 +58,27 @@ public class Main extends HttpServlet {
 			request.getRequestDispatcher("Search.jsp").forward(request, response);
 			return;
 		}
+		//手動添加keywords（不然讀不到）
+		keywords = new ArrayList<Keyword>();
+		Keyword keyworda = new Keyword("歌詞",0,4);
+		Keyword keywordb = new Keyword("作詞",0,3);
+		Keyword keywordc = new Keyword("字典",0,-2);
+		Keyword keywordd = new Keyword("新聞",0,-2);
+		Keyword keyworde = new Keyword("lyrics",0,4);
+		Keyword keywordf = new Keyword("MV",0,3);
+		
+		keywords.add(keyworda);
+		keywords.add(keywordb);
+		keywords.add(keywordc);
+		keywords.add(keywordd);
+		keywords.add(keyworde);
+		keywords.add(keywordf);
 		//連接到GoogleQuery class，進行搜尋
 		GoogleQuery google = new GoogleQuery(request.getParameter("keyword"), keywords);
 		HashMap<String, String> query = google.query();
 		
+		//query丟到googleitem跑出結果
+		//hashmap無法依序排放，這邊助教原本的hashmap key值為title
 		String[][] s = new String[query.size()][2];
 		request.setAttribute("query", s);
 		int num = 0;
@@ -72,6 +89,19 @@ public class Main extends HttpServlet {
 		    s[num][1] = value;
 		    num++;
 		}
+		
+		//如果要直接讀取webTreeList(排序過的）應該是這樣寫，但現在好像webListTree裡只有加到一個（詳細可以看google query那邊，我把回傳改成ArrayList方便有序排放
+		//另一個可能的解決辦法是用TreeMap然後直接拿算出來的分數當key值排放，但這樣就不用寫sorting，感覺有點作弊？
+		/*String[][] rs = new String[wbrs.size()][2];
+		request.setAttribute("query",rs);
+		int i=0;
+		
+		for(WebTree t: wbrs) {
+			rs[i][0]=t.root.nodeScore+"";
+			rs[i][1]=t.root.webPage.url;
+			i++;
+			System.out.println(i);
+		}*/
 		request.getRequestDispatcher("googleitem.jsp")
 		 .forward(request, response); 
 		
