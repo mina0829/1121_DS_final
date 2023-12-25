b530e862201ae099e50281857578c252c95b84d3
Linyi029 <118969781+Linyi029@users.noreply.github.com>
2023-12-25 19:35:22 +0800

    Update Main.java
    arraylist post到網頁上

--- DS_Final/src/Main.java
+++ DS_Final/src/Main.java
@@ -3,7 +3,10 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,6 +20,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.HttpStatusException;
+
 @WebServlet("/Main")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,28 +32,19 @@ public class Main extends HttpServlet {
     public Main() {
         super();
         // TODO Auto-generated constructor stub
-        /*try 
-		{
-			File file = new File("input.txt");
-			Scanner fileSc = new Scanner(file);
-			keywords = new ArrayList<Keyword>();
-			
-			if(fileSc.hasNext()) {
-				String name = fileSc.next();
-        		int weight = fileSc.nextInt();
-        		Keyword keyword = new Keyword(name, 0, weight);
-        		keywords.add(keyword);
-			}
-		}catch(Exception e) {
-			
-		}
-    }*/
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    }
+    
+    
+    //thoughts: 利用兩個hashmap,一個紀錄title, url 與分數，一個紀錄分數與排名，這樣是不是就可以做到有序插入顯示？
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
@@ -58,7 +54,6 @@ public class Main extends HttpServlet {
 			request.getRequestDispatcher("Search.jsp").forward(request, response);
 			return;
 		}
-		//手動添加keywords（不然讀不到）
 		keywords = new ArrayList<Keyword>();
 		Keyword keyworda = new Keyword("歌詞",0,4);
 		Keyword keywordb = new Keyword("作詞",0,3);
@@ -66,6 +61,7 @@ public class Main extends HttpServlet {
 		Keyword keywordd = new Keyword("新聞",0,-2);
 		Keyword keyworde = new Keyword("lyrics",0,4);
 		Keyword keywordf = new Keyword("MV",0,3);
+		Keyword keywordg = new Keyword("Music",0,3);
 		
 		keywords.add(keyworda);
 		keywords.add(keywordb);
@@ -73,40 +69,33 @@ public class Main extends HttpServlet {
 		keywords.add(keywordd);
 		keywords.add(keyworde);
 		keywords.add(keywordf);
+		keywords.add(keywordg);
+
+		
 		//連接到GoogleQuery class，進行搜尋
 		GoogleQuery google = new GoogleQuery(request.getParameter("keyword"), keywords);
-		HashMap<String, String> query = google.query();
 		
-		//query丟到googleitem跑出結果
-		//hashmap無法依序排放，這邊助教原本的hashmap key值為title
-		String[][] s = new String[query.size()][2];
-		request.setAttribute("query", s);
+		ArrayList <WebTree>wbrs = google.query();
+		
+		
 		int num = 0;
-		for(Entry<String, String> entry : query.entrySet()) {
-		    String key = entry.getKey();
-		    String value = entry.getValue();
-		    s[num][0] = key;
-		    s[num][1] = value;
-		    num++;
-		}
 		
-		//如果要直接讀取webTreeList(排序過的）應該是這樣寫，但現在好像webListTree裡只有加到一個（詳細可以看google query那邊，我把回傳改成ArrayList方便有序排放
-		//另一個可能的解決辦法是用TreeMap然後直接拿算出來的分數當key值排放，但這樣就不用寫sorting，感覺有點作弊？
-		/*String[][] rs = new String[wbrs.size()][2];
+		
+		String[][] rs = new String[wbrs.size()][2];
 		request.setAttribute("query",rs);
 		int i=0;
 		
 		for(WebTree t: wbrs) {
-			rs[i][0]=t.root.nodeScore+"";
+			rs[i][0]=t.root.webPage.name+"";
 			rs[i][1]=t.root.webPage.url;
 			i++;
-			System.out.println(i);
-		}*/
+		
+		}
+	
 		request.getRequestDispatcher("googleitem.jsp")
 		 .forward(request, response); 
-		
-	}
-
+		}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
