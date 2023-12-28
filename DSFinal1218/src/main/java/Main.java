366dab65dc5ce68c04ee8df055712efad09e653f
Linyi029 <118969781+Linyi029@users.noreply.github.com>
2023-12-28 15:28:13 +0800

    Update Main.java

--- DSFinal1218/src/main/java/Main.java
+++ DSFinal1218/src/main/java/Main.java
@@ -3,7 +3,10 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,6 +20,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.HttpStatusException;
+
 @WebServlet("/Main")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,33 +32,17 @@ public class Main extends HttpServlet {
     public Main() {
         super();
         // TODO Auto-generated constructor stub
-        
-    }
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    }
+    
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		try 
-		{
-		keywords = new ArrayList<Keyword>();
-		File file = new File("input.txt");
-		Scanner fileSc = new Scanner(file);
-		keywords = new ArrayList<Keyword>();
 		
-		while(fileSc.hasNext()) {
-			String name = fileSc.next();
-    		int weight = fileSc.nextInt();
-    		Keyword keyword = new Keyword(name,0,weight);
-    		
-    		keywords.add(keyword);
-
-
-		}
-	}catch(Exception e) {
-		
-	}
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
@@ -63,30 +52,52 @@ public class Main extends HttpServlet {
 			request.getRequestDispatcher("Search.jsp").forward(request, response);
 			return;
 		}
+		keywords = new ArrayList<Keyword>();
+		Keyword keyworda = new Keyword("歌詞",0,4);
+		Keyword keywordb = new Keyword("作詞",0,3);
+		Keyword keywordc = new Keyword("字典",0,-2);
+		Keyword keywordd = new Keyword("新聞",0,-2);
+		Keyword keyworde = new Keyword("lyrics",0,3);
+		Keyword keywordf = new Keyword("MV",0,3);
+		Keyword keywordg = new Keyword("Music",0,3);
+		Keyword keywordh = new Keyword("旅遊",0, -4);
+		Keyword keywordi = new Keyword("購物",0, -4);
 		
-		if(request.getParameter("keyword")!=null) {
-			Keyword newK = new Keyword(request.getParameter("keyword"),0,4);
-			keywords.add(newK);
-		};
+		keywords.add(keyworda);
+		keywords.add(keywordb);
+		keywords.add(keywordc);
+		keywords.add(keywordd);
+		keywords.add(keyworde);
+		keywords.add(keywordf);
+		keywords.add(keywordg);
+		keywords.add(keywordh);
+		keywords.add(keywordi);
+
+		
 		//連接到GoogleQuery class，進行搜尋
 		GoogleQuery google = new GoogleQuery(request.getParameter("keyword"), keywords);
-		HashMap<String, String> query = google.query();
 		
-		String[][] s = new String[query.size()][2];
-		request.setAttribute("query", s);
+		ArrayList <WebTree>wbrs = google.query();
+		
+		
 		int num = 0;
-		for(Entry<String, String> entry : query.entrySet()) {
-		    String key = entry.getKey();
-		    String value = entry.getValue();
-		    s[num][0] = key;
-		    s[num][1] = value;
-		    num++;
+		
+		
+		String[][] rs = new String[wbrs.size()][2];
+		request.setAttribute("query",rs);
+		int i=0;
+		
+		for(WebTree t: wbrs) {
+			rs[i][0]=t.root.webPage.name+"";
+			rs[i][1]=t.root.webPage.url;
+			i++;
+		
 		}
+	
 		request.getRequestDispatcher("googleitem.jsp")
 		 .forward(request, response); 
-		
-	}
-
+		}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
